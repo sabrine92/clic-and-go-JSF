358c5de2a75437b40132ec09e3f31d602f215ff8
Sabrine <Sabrine@172.16.11.42>
2016-01-13 09:36:33 +0100

    nadia
    Signed-off-by: Sabrine <Sabrine@172.16.11.42>

--- clic-and-go/clic-and-go-web/src/main/java/beans/MeanofTransportConverter.java
+++ clic-and-go/clic-and-go-web/src/main/java/beans/MeanofTransportConverter.java
@@ -1,46 +1,37 @@
 package beans;
 
-
-	
-
-	import javax.ejb.EJB;
+import javax.ejb.EJB;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
-import javax.faces.convert.FacesConverter;
 
-import entities.MeanOfTransport;
 import services.interfaces.MeanOfTransportServicesLocal;
+import entities.MeanOfTransport;
 
-
-
-
-public class MeanofTransportConverter  implements Converter {
+public class MeanofTransportConverter implements Converter {
 
 	@EJB
-	private MeanOfTransportServicesLocal MeanOfTransportServicesLocal;	
-		@Override
-		public Object getAsObject(FacesContext context, UIComponent component,String value) {
-				MeanOfTransport mean = null;
-				System.out.println(MeanOfTransportServicesLocal);
-				mean = MeanOfTransportServicesLocal.findMeanOfTransportByName(value);
-				return mean;
-		}
+	private MeanOfTransportServicesLocal MeanOfTransportServicesLocal;
 
-		@Override
-		public String getAsString(FacesContext context, UIComponent component,
-				Object value) {
-			String typecar = null;
-			if (value == null || value.equals("")) {
-				typecar="";
-			}else {
-			typecar = ((MeanOfTransport)value).getRegistrationNumber();
-			}
-			return typecar;
-		}
-		
-
-
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String value) {
+		MeanOfTransport mean = null;
+		System.out.println(MeanOfTransportServicesLocal);
+		mean = MeanOfTransportServicesLocal.findMeanOfTransportByName(value);
+		return mean;
 	}
 
+	@Override
+	public String getAsString(FacesContext context, UIComponent component,
+			Object value) {
+		String typecar = null;
+		if (value == null || value.equals("")) {
+			typecar = "";
+		} else {
+			typecar = ((MeanOfTransport) value).getRegistrationNumber();
+		}
+		return typecar;
+	}
 
+}
