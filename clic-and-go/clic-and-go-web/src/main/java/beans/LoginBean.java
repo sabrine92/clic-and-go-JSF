d5274017ab013f446e9279e5745e2ab38ff21252
Sabrine <Sabrine@172.16.11.42>
2016-01-13 04:27:21 +0100

    Login + khedmet nadia
    Signed-off-by: Sabrine <Sabrine@172.16.11.42>

--- clic-and-go/clic-and-go-web/src/main/java/beans/LoginBean.java
+++ clic-and-go/clic-and-go-web/src/main/java/beans/LoginBean.java
@@ -1,8 +1,10 @@
 package beans;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 import services.interfaces.SessionLocal;
 import services.interfaces.UserServicesLocal;
@@ -13,28 +15,41 @@ import entities.User;
 @SessionScoped
 public class LoginBean {
 	private User user = new User();
+	private Boolean cm = true;
+	private Boolean LoggedIn=false;
 	@EJB
 	private UserServicesLocal userServicesLocal;
 	@EJB
 	private SessionLocal sessionLocal;
 
 	public String doLogin() {
+		setLoggedIn(false);
 		String navigateTo = "";
 		User userLoggedIn = userServicesLocal.authenticate(user.getName(),
 				user.getPassword());
 		System.out.println("userLoggedIn :" + userLoggedIn);
 		if (userLoggedIn != null) {
+			setLoggedIn(true);
 			user = userLoggedIn;
 			sessionLocal.setLogin(userLoggedIn.getName());
 			sessionLocal.setPwd(userLoggedIn.getPassword());
 			if (userLoggedIn instanceof Traveler) {
+				cm = false;
 				System.out.println(userLoggedIn instanceof Traveler);
-				navigateTo = "pages/cm/cmHome";
+				navigateTo = "pages/traveler/trHome?faces-redirect=true";
+			}else{
+				cm=true;
+				navigateTo="pages/cm/cmHome?faces-redirect=true";
 			}
 		} else {
-			navigateTo = "error";
+			 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Wrong credentials"));
+			
 		}
 		return navigateTo;
+	}
+	
+	public Boolean isLoggedIn(){
+		return LoggedIn;
 	}
 
 	public User getUser() {
@@ -43,6 +58,22 @@ public class LoginBean {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	public Boolean getCm() {
+		return cm;
+	}
+
+	public void setCm(Boolean cm) {
+		this.cm = cm;
+	}
+	
+	public Boolean getLoggedIn() {
+		return LoggedIn;
+	}
+
+	public void setLoggedIn(Boolean loggedIn) {
+		LoggedIn = loggedIn;
 	}
 
 }
